70efda2a81ecec63b921bf3ab425fe3100273cb0
Roberta Drayer <robertadrayer@gmail.com>
2023-12-10 16:04:18 -0800

    added auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,33 @@
+const jwt = require("jsonwebtoken");
+const Router = require("express").Router;
+const router = new Router();
+
+const User = require("../models/user");
+const { SECRET_KEY } = require("../config");
+const ExpressError = require("../expressError");
+
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
+router.post('/login', async function (req, res, next) {
+    try {
+        let {username, password} = req.body;
+        if (await User.authenticate(username, password)) {
+            let token = jwt.sign({username}, SECRET_KEY);
+            User.updateLoginTimestamp(username);
+            return res.json({token});
+        } else {
+            throw new ExpressError("invalid username/password", 400)
+        }
+    }
+    catch (e) {
+        return next(e);
+    }
+});
+
 
 
 /** POST /register - register user: registers, logs in, and returns token.
@@ -11,3 +36,19 @@
  *
  *  Make sure to update their last-login!
  */
+router.post('/register', async function (req, res, next) {
+    try {
+        let { username, password, first_name, last_name, phone } = req.body;
+        if (!username || !password || !first_name || !last_name || !phone) {
+            throw new ExpressError("please fill in all fields", 400)
+        }
+        if (await User.register(username, password, first_name, last_name, phone)) {
+            let token = jwt.sign({username}, SECRET_KEY);
+            User.updateLoginTimestamp(username);
+            return res.json({token});
+        }
+    }
+    catch (e) {
+        return next(e);
+    }
+})
